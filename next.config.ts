0922b374b009af6ddf333c82ada142ec848205f6
Deepesh Rijal <deepeshrijal@gmail.com>
2025-11-16 15:58:47 +0545

    image not loading issue fiox

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,13 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "*.s3.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "virtualtrialroombackendst-virtualtryonresultsbucke-r3w9znvctxpc.s3.us-east-1.amazonaws.com",
+      },
     ],
+    // Disable image optimization for development (optional)
+    unoptimized: process.env.NODE_ENV === "development",
   },
 };
 
